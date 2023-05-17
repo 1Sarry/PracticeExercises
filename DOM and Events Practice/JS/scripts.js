d5dbcb0d4a3d2dc5fd5f44eb2e4802b5610faa61
1Sarry <saronmeles4@gmail.com>
2023-05-17 14:58:33 +0300

    Question 1 Done!

---
+++ DOM and Events Practice/JS/scripts.js
@@ -0,0 +1,24 @@
+// alert("We have just started DOM!")
+
+
+// Question 1
+
+const elem = document.getElementById("sample1");
+//console.log(elem)
+//console.log(elem.innerHTML)
+//console.log(elem.textContent)
+
+
+// Question 2
+
+const companies = document.getElementById("techCompanies")
+
+console.log(companies)
+
+// Question 3
+
+
+
+
+
+// Question 4
