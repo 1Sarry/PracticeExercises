18aef8b85c37ddb9369c4f4fce0d94e0c3d3f9dd
1sarry <saronmeles4@gmail.com>
2023-05-19 01:48:05 +0300

    2.4

--- DOM and Events Practice/JS/scripts.js
+++ DOM and Events Practice/JS/scripts.js
@@ -11,10 +11,33 @@ const elem = document.getElementById("sample1");
 
 // Question 2
 
-const companies = document.getElementById("techCompanies")
+//2.1
+// const companies = document.getElementById("techCompanies")
+// console.log(companies)
 
-console.log(companies)
+//2.2
+// const companies = document.querySelector("#techCompanies")
+// console.log(companies)
 
+// var companies = document.querySelectorAll("li")
+// console.log(companies)
+
+// const redClass = document.querySelectorAll(".red")
+// console.log(redClass)
+
+//2.3 
+// const company = document.getElementById("techCompanies")
+// const companies = company.querySelectorAll('ul li ');
+// console.log(companies)
+
+
+//2.4
+const company = document.querySelectorAll('.red')
+ //const classRed = company.getElementsByClassName(".red")
+console.log(company)
+
+const classRed = document.getElementsByClassName("red")
+console.log(classRed)
 // Question 3
 
 
