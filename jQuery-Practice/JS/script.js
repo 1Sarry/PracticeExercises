58441019fbee182cee30aca5cb694c4a07f7b423
1Sarry <saronmeles4@gmail.com>
2023-05-25 18:06:26 +0300

    JQ ctnd...

---
+++ jQuery-Practice/JS/script.js
@@ -0,0 +1,12 @@
+/* Q-1
+1.1. Select the element with an id of "sample1" using jQuery.
+1.2. Print the element itself on the console upon page refresh.
+1.3. Print the content of the element on the console upon page refresh. Use jQuery to select
+the content of the element
+*/
+
+var x = $("#sample1")
+console.log(x)
+
+var y = $("#sample1 div")
+console.log(y)
